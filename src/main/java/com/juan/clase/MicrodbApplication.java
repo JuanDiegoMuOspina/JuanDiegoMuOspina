2bcf2c9e66becebb599687234cd2401b2541f7d3
Juan Diego <jdmunozo_1@uqvirtual.edu.co>
2022-08-25 17:30:30 -0500

    persistiendo BD

---
+++ src/main/java/com/juan/clase/MicrodbApplication.java
@@ -0,0 +1,13 @@
+package com.juan.clase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrodbApplication.class, args);
+	}
+
+}
